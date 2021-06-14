e3f49593864a517ef0c80721406ced0c3d8bf59c
2Zero1 <Good7219@hotmail.com>
2021-06-14 20:02:31 +0900

    Set webpack config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.jsx'),
+  module: {
+    rules: [
+      {
+        test: /\.(jsx)?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+};
